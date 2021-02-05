aeda6120b9c4c93df8dc935d36213b89b7030ad5
Lennard Alms <mail@lennardalms.de>
2021-02-05 16:41:38 +0100

    auto_update

--- preprocessing/BuildDB.py
+++ preprocessing/BuildDB.py
@@ -27,30 +27,36 @@ def buildDB(paths, patch_sizes=[(200,200),(400,400)], overlap=0.5, signature_siz
 
         # record time for animation eta
         start_time = time.time()
-
+        print('exit0')
         # make a test run to find output shape for each image this implies that all images have the same dimensions
         test_run = get_patches_from_image(cv2.imread(paths[0]), patch_size, overlap)
         test_pred = tf.convert_to_tensor(test_run[:2], dtype=test_run.dtype)
         test_pred = vgg.predict(tf.keras.applications.vgg16.preprocess_input(test_pred), verbose=0)
+        print('exit1')
 
         # save the patches per image so we can do a back calculation later and see what patch came from what image
         patches_per_image = test_run.shape[0]
         patches_per_image_list.append(patches_per_image)
+        print('exit2')
 
         # initialize ETA
         eta = round((time.time() - start_time) * len(paths) * 2 / 60, 2)
+        print('exit3')
 
         # calculate the hyperplane normals for hashing
         pred_dim = test_pred.shape[1] * test_pred.shape[2] * test_pred.shape[3]
         hyperplane_normals = np.random.normal(0,1,(pred_dim,signature_size))
+        print('exit4')
 
         # save hyperplane normals for prediction later
         hyperplane_normals_list.append(hyperplane_normals)
+        print('exit5')
 
         # free the variable since we have no further use for it
         del(test_run)
         del(test_pred)
         gc.collect()
+        print('exit6')
 
         # record time for animation eta
         start_time = time.time()
@@ -64,13 +70,16 @@ def buildDB(paths, patch_sizes=[(200,200),(400,400)], overlap=0.5, signature_siz
             # calculate the range of images
             path_idx_start = batch_size * batch_idx
             path_idx_end = path_idx_start + batch_size
+            print('exit7')
 
             # load the images and create the patches
             patches = np.concatenate([get_patches_from_image(cv2.imread(path), patch_size, overlap) for path in paths[path_idx_start:path_idx_end]])
+            print('exit8')
 
             # use vgg to calculate the feature vectors
             patches = tf.convert_to_tensor(patches, dtype=patches.dtype)
             patches = vgg.predict(tf.keras.applications.vgg16.preprocess_input(patches), verbose=1)
+            print('exit9')
 
             # flatten the feature vectors
             patches = patches.reshape((patches.shape[0],pred_dim))
@@ -101,22 +110,23 @@ def buildDB(paths, patch_sizes=[(200,200),(400,400)], overlap=0.5, signature_siz
             del(patches)
             gc.collect()
 
-        # save ppi and hash normals
-        patches_per_image_list = np.array(patches_per_image_list)
-        hyperplane_normals_list = np.array(hyperplane_normals_list)
-        with h5py.File("hashes.hdf5", "w") as f:
-            hfile = f.create_dataset('ppi', patches_per_image_list.shape , dtype='i')
-            hfile[:] = patches_per_image_list
-            hfile = f.create_dataset('hn', hyperplane_normals_list.shape , dtype='f')
-            hfile[:] = patches_per_image_list
         # free variables
         del(vgg)
-        del(patches_per_image_list)
-        del(hyperplane_normals_list)
         gc.collect()
 
+    # save ppi and hash normals
+    patches_per_image_list = np.array(patches_per_image_list)
+    hyperplane_normals_list = np.array(hyperplane_normals_list)
+    with h5py.File("hashes.hdf5", "w") as f:
+        hfile = f.create_dataset('ppi', patches_per_image_list.shape , dtype='i')
+        hfile[:] = patches_per_image_list
+        hfile = f.create_dataset('hn', hyperplane_normals_list.shape , dtype='f')
+        hfile[:] = patches_per_image_list
 
-
+    # free variables
+    del(patches_per_image_list)
+    del(hyperplane_normals_list)
+    gc.collect()
 
 
 
