d0907c61d5cf608fbfafff994777d88248fef1be
Markus Laubenthal <mail@markuslaubenthal.de>
2021-02-05 20:10:27 +0100

    evaluation fix

--- postprocessing/ErrorEvaluation.py
+++ postprocessing/ErrorEvaluation.py
@@ -2,25 +2,34 @@ import numpy as np
 
 
 def calculateError(j_sims, j_index, positive_indices, steps=100):
-    sim = 0.365
-    TP,TN,FP,FN = 0,0,0,0
     space = np.linspace(j_sims[-1],j_sims[0],steps)
-    for i in range(len(j_index)):
-        if j_sims[i] >= sim:
-            if j_index[i] in positives:
-                TP += 1
+    precisions = []
+    accuracies = []
+    recalls = []
+    for sim in space:
+        TP,TN,FP,FN = 0,0,0,0
+        for i in range(len(j_index)):
+            if j_sims[i] >= sim:
+                if j_index[i] in positives:
+                    TP += 1
+                else:
+                    FP += 1
             else:
-                FP += 1
-        else:
-            if j_index[i] in positives:
-                FN += 1
-            else:
-                TN += 1
-    precision = TP/(TP+FP)
-    recall = (TP/(TP+FN))
-    accuracy = ((TP + TN)/(TP + TN + FP + FN))
-    print("recall,", "precision,", "accuracy,", recall, precision, accuracy)
-    print("TP, FP, FN, TN", TP, FP, FN, TN)
+                if j_index[i] in positives:
+                    FN += 1
+                else:
+                    TN += 1
+
+        precision = TP/(TP+FP)
+        recall = (TP/(TP+FN))
+        accuracy = ((TP + TN)/(TP + TN + FP + FN))
+        precisions.append(precision)
+        recalls.append(recalls)
+        accuracies.append(accuracy)
+
+    return recalls, precisions, accuracies, space
+    # print("recall,", "precision,", "accuracy,", recall, precision, accuracy)
+    # print("TP, FP, FN, TN", TP, FP, FN, TN)
 
 
 def jacc_sim_calc(H, Q):
