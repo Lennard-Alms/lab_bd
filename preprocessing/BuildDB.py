c1936d2673b8bf4b1f886cac939b2ed900fea34e
Lennard Alms <mail@lennardalms.de>
2021-02-05 18:12:25 +0100

    auto_update

--- preprocessing/BuildDB.py
+++ preprocessing/BuildDB.py
@@ -108,6 +108,8 @@ def buildDB(paths, patch_sizes=[(200,200),(400,400)], overlap=0.5, signature_siz
     # save ppi and hash normals
     patches_per_image_list = np.array(patches_per_image_list)
     hyperplane_normals_list = np.concatenate(hyperplane_normals_list)
+    print(hyperplane_normals_list.shape)
+    print(hyperplane_normals_list)
     with h5py.File("hashes.hdf5", "a") as f:
         if 'ppi' in f:
             hfile = f['ppi']
