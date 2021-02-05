2ac3a387e8b1aa5d640899f63b94f862eae399c5
Lennard Alms <mail@lennardalms.de>
2021-02-05 18:40:18 +0100

    auto_update

--- preprocessing/BuildQuery.py
+++ preprocessing/BuildQuery.py
@@ -14,37 +14,30 @@ def buildQuery(paths, patch_sizes=[(200,200),(400,400)]):
 
     for normals_index, patch_size in enumerate(patch_sizes):
         print("Size " + str(patch_size))
-        print('exit0')
 
         # select hyperplane normals for hashing
         with h5py.File("hashes.hdf5", "a") as f:
             set_name = 'hn' + str(patch_size)
             hyperplane_normals = f[set_name][:]
-        print('exit1')
-        print(hyperplane_normals.shape)
 
         # vgg needs a specific input shape thats why we declare it inside the patch loop
         vgg = tf.keras.applications.VGG16(include_top=False,
                                       weights='imagenet',
                                       input_shape=(patch_size[0], patch_size[1], 3))
-        print('exit2')
 
         # generate patches via rescaling
         patches = np.array([cv2.resize(img, patch_size) for img in images])
-        print('exit3')
 
         # use vgg to calculate the feature vectors
         patches = tf.convert_to_tensor(patches, dtype=patches.dtype)
         patches = vgg.predict(tf.keras.applications.vgg16.preprocess_input(patches), verbose=1)
-        print('exit4')
 
         # flatten the feature vectors
         patches = patches.reshape((patches.shape[0],-1))
-        print('exit5')
 
         # calculate the hash signatures
         patches = np.dot(patches, hyperplane_normals) < 0
-        print('exit6')
+
         # save in file with option "a" => read write if exists esle create
         set_name = 'Q' + str(patch_size)
         with h5py.File("hashes.hdf5", "a") as f:
@@ -54,7 +47,6 @@ def buildQuery(paths, patch_sizes=[(200,200),(400,400)]):
                 hfile = f.create_dataset(set_name, patches.shape , dtype='?')
             # save the calculated hashes in the h file dataset
             hfile[:] = patches
-        print('exit7')
 
         # free variables
         del(patches)
