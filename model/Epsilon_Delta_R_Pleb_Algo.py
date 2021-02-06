3653f4899da020b16ee767d6bfa005e3dcca8617
Lennard Alms <mail@lennardalms.de>
2021-02-06 19:46:50 +0100

    quick update

---
+++ model/Epsilon_Delta_R_Pleb_Algo.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+class EDRP_Algo:
+    def __init__(self, n, eps=2, d1=0.5):
+        #eps = 2
+        #d1 = np.pi / 6
+        d2 = eps * d1
+        p1 = 1 - d1 / np.pi
+        p2 = 1 - d2 / np.pi
+        k = int(math.ceil(- np.log2(n) / np.log2(p2)))
+        roh = np.log2(p1) / np.log2(p2)
+        L = int(np.ceil((n ** roh) / p1))
+        delta = 1/np.e
+        print('With probability ' + round(1-delta,2) + ' this will return at least one epsilon nearest neighbour if one exists.')
+        self.d2 = d2
+        self.k = k
+        self.L = L
+        self.signature_size = k*L
+
+    def create_hash_tables(self, hashes):
+        self.T = []
+        for l in range(self.L):
+          t = {}
+          for p in range(hashes.shape[0]):
+            hash_key = str(hashes[p,k*l:k*(l+1)])
+            if hash_key in t:
+              t[hash_key].append(p)
+            else:
+              t[hash_key] = [p]
+          self.T.append(t)
+
+    def query_for(self, query):
+        colisions = set()
+        for l in range(L):
+          hash_key = str(query[k*l:k*(l+1)])
+          for p in T[l][hash_key]:
+            colisions.add(p)
+        return list(colisions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #
