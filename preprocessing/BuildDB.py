5c03233fc91ba7d36494b59b480478149d394265
Lennard Alms <mail@lennardalms.de>
2021-02-23 20:40:53 +0100

    quick update

--- preprocessing/BuildDB.py
+++ preprocessing/BuildDB.py
@@ -151,13 +151,13 @@ def buildDB(paths, patch_sizes=[(200,200),(400,400)], overlap=0.5, signature_siz
 def extract_features(images, model, batch_size=20, network_batch_size=5):
     features_collection = []
 
-    max_batches = math.ceil(len(image_paths) / batch_size)
+    max_batches = math.ceil(len(images) / batch_size)
 
     for batch_idx in range(max_batches):
       path_idx_start = batch_size * batch_idx
       path_idx_end = path_idx_start + batch_size
 
-      images = np.array([get_image(path) for path in image_paths[path_idx_start:path_idx_end]])
+      images = np.array([get_image(im) for im in images[path_idx_start:path_idx_end]])
 
       features = model.predict(tf.keras.applications.vgg19.preprocess_input(images), verbose=1, batch_size=network_batch_size)
       features_collection.append(features)
